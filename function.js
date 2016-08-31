b54c7e83ce370b1ccc8909bdc9fc61796421658f
mueedahmed1 <goodiuck_2010@yahoo.com>
2016-08-31 21:13:42 +0500

    task finished

---
+++ function.js
@@ -0,0 +1,12 @@
+/**
+ * Created by User on 8/29/2016.
+ */
+
+$(document).ready(function(){
+  $('.sliderr').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: false
+      //autoplaySpeed: 4000
+})
+});
